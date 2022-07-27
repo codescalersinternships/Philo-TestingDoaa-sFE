a5967ce541cfbcd8e63067936ca73d8be3f9bbd6
AmiraHisham20 <amirahisham.258@gmail.com>
2022-07-27 09:58:47 +0200

    done

--- main_test.go
+++ main_test.go
@@ -3,30 +3,91 @@ package todo
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
+	//"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestCreateTodo(t *testing.T) {
-	var server Server
-	newTodo := todos{
+	var s Server
+	newtodo := todos{
+		ID:   40,
+		Task: "task40",
+	}
+	s.InitializeDB()
+	value, _ := json.Marshal(newtodo)
+
+	t.Run("create new todo list ", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/todo", bytes.NewBuffer(value))
+		response := httptest.NewRecorder()
+		s.CreateTodo(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+
+	})
+}
+func TestGetTodo(t *testing.T) {
+	var s Server
+	s.InitializeDB()
+	newtodo := todos{
+		ID:   3,
+		Task: "task3",
+	}
+	value, _ := json.Marshal(newtodo)
+
+	t.Run("Get all todo tasks", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/todo", bytes.NewBuffer(value))
+		response := httptest.NewRecorder()
+		s.Gettodo(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+func TestGetTodoById(t *testing.T) {
+	var s Server
+	s.InitializeDB()
+	newtodo := todos{
+		ID:   20,
+		Task: "task20",
+	}
+	value, _ := json.Marshal(newtodo)
+
+	t.Run("Get todo tasks by id", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/todo/20", bytes.NewBuffer(value))
+		response := httptest.NewRecorder()
+		s.Gettodobyid(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+}
+func TestUpdateTodob(t *testing.T) {
+	var s Server
+	newtodo := todos{
 		ID:   12,
 		Task: "task",
 	}
-	server.InitializeDB()
-	jsonValue, _ := json.Marshal(newTodo)
+	s.InitializeDB()
+	value, _ := json.Marshal(newtodo)
 
-	t.Run("create new todo", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodPost, "/todo", bytes.NewBuffer(jsonValue))
+	t.Run("Get todo tasks by id", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/todo/12", bytes.NewBuffer(value))
 		response := httptest.NewRecorder()
-	
-		server.CreateTodo(response, request)
-
+		s.UpadateTodo(response, request)
 		assertStatus(t, response.Code, http.StatusOK)
-		fmt.Println("here4")
+	})
+}
+func TestDeleteTodob(t *testing.T) {
+	var s Server
+	newtodo := todos{
+		ID:   12,
+		Task: "task",
+	}
+	s.InitializeDB()
+	value, _ := json.Marshal(newtodo)
 
+	t.Run("Get toto tasks by id", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/todo/12", bytes.NewBuffer(value))
+		response := httptest.NewRecorder()
+		s.DeleteTodo(response, request)
+		assertStatus(t, response.Code, http.StatusAccepted)
 	})
 }
 
