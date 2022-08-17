7283228033f88599286a21338d514fce75e1afd9
Philopateer Fouad <Philopateeramgad17@hotmail.com>
2022-08-17 16:46:29 +0200

    base URL

--- svelte-typescript-app/cypress/e2e/todolist.cy.js
+++ svelte-typescript-app/cypress/e2e/todolist.cy.js
@@ -42,7 +42,7 @@ describe("Doaa", function () {
   });
   //this next test is the way it is as sometimes requests fail when text is entered in a JSON object format,
   //this is trying to make it fail, I am not trying to enter an actual JSON object
-  // its like when entering an SQL query into a password texrbox to make it fail
+  //its like when entering an SQL query into a password texrbox to make it fail
   it("create item-JSON as text", () => {
     cy.get("#inputID").type("93");
     cy.get("#inputTask").type('{ID:15, task:"JSON example"}');
@@ -77,29 +77,3 @@ describe("Doaa", function () {
     cy.get('[type="get"]').click();
   });
 });
-
-//   //open target page
-//   it("sign up", () => {
-//     cy.visit("https://www.way2automation.com/");
-//     //cy.visit("https://sso.teachable.com/secure/673/identity/login");
-//     cy.get("li[id='menu-item-27625'] span[class='menu-text']").click();
-
-//     cy.get(".heading3").contains("Log in");
-//     cy.get("a[href='/secure/673/identity/sign_up']").click();
-
-//     cy.get(".container.m-auto-xs.col-4-l.col-5-m.col-10-xs.p-h-6-l")
-//       .find(".heading3.text-center-xs.m-b-0-xs.m-b-4-m")
-//       .contains("Sign Up");
-//     cy.get(".container.m-auto-xs.col-4-l.col-5-m.col-10-xs.p-h-6-l")
-//       .find("#user_name")
-//       .type("TestCypress");
-
-//     cy.get("#user_email").type("mail@email.com");
-
-//     cy.get("#password").type("Password");
-
-//     cy.get(":nth-child(6) > .dsp-flex-xs").find(".m-l-0-xs").click();
-
-//     cy.get('[data-testid="signup-button"]').click();
-//   });
-// });
